bd0c1ec082c6ffd2a5ac8cb74103ba7956236ef1
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-12 09:18:29 +0530

    Repeat till the position is 100

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -6,28 +6,30 @@ public class SnakeandLadder
 	public static void main(String[] args)
 	{
 		int position=0;
-		int [] board=new int [101];
-		int die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
-		
-		int option=(int)Math.floor((Math.random()*10 +1 )% 3);
-		switch(option) 
+	//	int [] board=new int [101];
+		while(position==100) 
 		{
-		case 0:
-				break;
-		case 1:
-				position=position+die_value;
-				break;
-		case 2:
-				if(position>die_value && position>0) 
-				{
-					position=position-die_value;
+			int die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
+			int option=(int)Math.floor((Math.random()*10 +1 )% 3);
+			switch(option) 
+			{
+			case 0:
 					break;
-				}
-				else
-				{
-					position=0;
+			case 1:
+					position=position+die_value;
 					break;
-				}
+			case 2:
+					if(position>die_value && position>0) 
+					{
+						position=position-die_value;
+						break;
+					}
+					else
+					{
+						position=0;
+						break;
+					}
+			}
 		}
 	}
 }
