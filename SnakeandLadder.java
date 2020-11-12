d2808c6b40e6f5f2ac9c3e47f74d5e0ddd7b177c
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-12 08:50:27 +0530

    Getting started with snakes and ladder

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,14 +1,11 @@
 package day3;
 
-public class SnakeandLadder {
+public class SnakeandLadder
+{
 
 	public static void main(String[] args)
 	{
-		int position=0;
-		int [] board=new int [101];
-		int die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
-		
-
+		System.out.println("getting started with Snakke and Ladder");
 	}
 
 }
