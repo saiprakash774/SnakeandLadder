4a7c8f7f56704bf386b0727efa022abf0eb35f76
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-12 09:09:14 +0530

    Player checks for option

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,14 +1,33 @@
 package day3;
 
-public class SnakeandLadder {
+public class SnakeandLadder 
+{
 
 	public static void main(String[] args)
 	{
 		int position=0;
 		int [] board=new int [101];
-		int die_value=(int)Math.floor((Math.random()*10 +1 )% 10);
+		int die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
 		
-
+		int option=(int)Math.floor((Math.random()*10 +1 )% 3);
+		switch(option) 
+		{
+		case 0:
+				break;
+		case 1:
+				position=position+die_value;
+				break;
+		case 2:
+				if(position>die_value && position>0) 
+				{
+					position=position-die_value;
+					break;
+				}
+				else
+				{
+					position=0;
+					break;
+				}
+		}
 	}
-
 }
