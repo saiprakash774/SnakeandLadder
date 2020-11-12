ab2b144c0616e82ac9f0e439cd34eb27d432d351
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-12 08:55:08 +0530

    Player at zero position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -5,7 +5,8 @@ public class SnakeandLadder
 
 	public static void main(String[] args)
 	{
-		System.out.println("getting started with Snakke and Ladder");
+		int position=0;
+		int [] board=new int [101];
 	}
 
 }
