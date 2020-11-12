94c62f3c5d85471145b9ded302816841f6114b33
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-12 08:34:55 +0530

    Player roll the die UC-2

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -2,10 +2,11 @@ package day3;
 
 public class SnakeandLadder {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		int position=0;
 		int [] board=new int [101];
-		board[0]=position;
+		int die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
 		
 
 	}
