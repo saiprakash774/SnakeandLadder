0f685c01ed7eae90da505fe2de4a52bda4a75af9
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-12 21:40:00 +0530

    Implemented two player Game

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,45 +1,68 @@
 package day3;
-
 public class SnakeandLadder 
 {
-
-	public static void main(String[] args)
-	{
-		int position=0;
-		int no_of_die_rolls=0;
-		while(position<100) 
+	private static int position_of_player1=0;
+	private static int position_of_player2=0;
+	private static int no_of_die_rolls=0;
+	public static int play(int value) 
+	 {
+		if(value<100) 
 		{
 			int die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
-			System.out.println("number of die roll "+no_of_die_rolls+" position of player:"+position);
 			no_of_die_rolls=no_of_die_rolls+1;
-			int option=(int)Math.floor((Math.random()*10 +1 )% 3);
+			int option=(int)Math.floor((Math.random()*10)% 3);
 			switch(option) 
 			{
 			case 0:
 					break;
-			case 1:
-					if(position+die_value<=100) 
+			case 1: //Ladder
+					if(value+die_value<=100) 
 					{
-						position=position+die_value;
+						value=value+die_value;
+						if(value+die_value<=100)
+						{
+							die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
+							value=value+die_value;
+						}
 						break;
+						
 					}
 					else
 					{
 						break;
 					}
-			case 2:
-					if(position>die_value && position>0) 
+					
+			case 2:  //Snake
+					if(value>die_value && value>0) 
 					{
-						position=position-die_value;
+						value=value-die_value;
 						break;
 					}
 					else
 					{
-						position=0;
+						value=0;
 						break;
 					}
+			default:
+					System.out.println("Something unexpected happen: ");
 			}
 		}
-		System.out.println("number of die roll "+no_of_die_rolls+" position of player:"+position);
+		return value;
+	 }
+	public static void main(String[] args)
+	{
+		while(position_of_player1<100 && position_of_player2<100) 
+		{
+			position_of_player1=play(position_of_player1);
+			System.out.println("number of die roll "+no_of_die_rolls+" position of player1: "+position_of_player1);
+			position_of_player2=play(position_of_player2);	
+			System.out.println("number of die roll "+no_of_die_rolls+" position of player2: "+position_of_player2);
+		}
+		if(position_of_player1==100)
+			System.out.println("player 1 is winner");
+		else
+			System.out.println("player2 is winner");
+		
 	}
+	
 }
